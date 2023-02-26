6f6b7c26bbff0f8dcd570636cefc6477e8bc4c52
chris315654 <42459246+chris315654@users.noreply.github.com>
2023-02-26 13:51:50 -0800

    remake project
    naming might be problematic

--- DrZot/DrZotTests/DrZotTests.swift
+++ DrZot/DrZotTests/DrZotTests.swift
@@ -1,14 +1,14 @@
 //
-//  Dr__ZotTests.swift
-//  Dr. ZotTests
+//  DrZotTests.swift
+//  DrZotTests
 //
 //  Created by Chris Ruan on 2/26/23.
 //
 
 import XCTest
-@testable import Dr__Zot
+@testable import DrZot
 
-final class Dr__ZotTests: XCTestCase {
+final class DrZotTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- DrZot/DrZotUITests/DrZotUITests.swift
+++ DrZot/DrZotUITests/DrZotUITests.swift
@@ -1,13 +1,13 @@
 //
-//  Dr__ZotUITests.swift
-//  Dr. ZotUITests
+//  DrZotUITests.swift
+//  DrZotUITests
 //
 //  Created by Chris Ruan on 2/26/23.
 //
 
 import XCTest
 
-final class Dr__ZotUITests: XCTestCase {
+final class DrZotUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
